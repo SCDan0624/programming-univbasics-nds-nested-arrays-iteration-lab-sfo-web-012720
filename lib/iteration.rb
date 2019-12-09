beebfc50ec1bc1fec6ae04c12fafcc081345dacb
Daniel Mayle <dmayle012@gmail.com>
2019-12-09 17:46:12 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -8,7 +8,8 @@ def join_ingredients(src)
   # As such, there should be a new String for each inner array, or pair
   
   row_index = 0 
-  while row_index < src.length 
+  while row_index < src.length do 
+  end
 end
 
 def find_greater_pair(src)
