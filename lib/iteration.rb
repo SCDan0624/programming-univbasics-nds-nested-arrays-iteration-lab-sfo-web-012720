208f4d2d8c380ab4912cc729b6abd795da6ee5ca
Daniel Mayle <dmayle012@gmail.com>
2019-12-09 17:46:42 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -10,7 +10,8 @@ def join_ingredients(src)
   row_index = 0 
   while row_index < src.length do 
     element_index = 0 
-    while element_index < src[row_index]
+    while element_index < src[row_index].length do 
+      
   end
 end
 
